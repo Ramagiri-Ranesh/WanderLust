2619d160422731f0c8b45ed988a655c2f1a6b909
Ramagiri-Ranesh <218R1A12B6@cmrec.ac.in>
2025-08-16 14:31:05 +0530

    Handling Errors with middlewares

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const Listing = require('./models/listing');
 const methodOverride = require('method-override')
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync');
+const ExpressError = require('./utils/ExpressError');
 const app = express();
 const port = 8080;
 
@@ -36,10 +38,10 @@ app.get("/",(req,res) => {
 });
 
 // Index Route
-app.get("/listings", async (req,res) => {
+app.get("/listings", wrapAsync(async (req,res) => {
     const  allListings =  await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-});
+}));
 
 // New Route
 app.get("/listings/new",(req,res) => {
@@ -47,40 +49,52 @@ app.get("/listings/new",(req,res) => {
 });
 
 // Create Route
-app.post("/listings", async (req,res) => {
+app.post("/listings", wrapAsync(async (req,res) => {
     // let {title, description, image, price, location, country} = req.body; alternative create a array
     // let listing = req.body.listings;
     const newListing =  new Listing(req.body.listings);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 // Show Route
-app.get("/listings/:id", async (req,res) => {
+app.get("/listings/:id", wrapAsync(async (req,res) => {
     let { id } = req.params;
     const listing =  await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-});
+}));
 
 // Edit Route
-app.get("/listings/:id/edit", async (req,res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-});
+}));
 
 // Update Route
-app.put("/listings/:id", async (req,res) => {
+app.put("/listings/:id", wrapAsync(async (req,res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id , { ...req.body.listings }, {runValidators : true});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 // Delete Route
-app.delete("/listings/:id", async (req,res) => {
+app.delete("/listings/:id", wrapAsync(async (req,res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+// if it dose n't match any route
+// Catch-all 404 (works in Express 5)
+app.all(/.*/, (req, res, next) => {
+    next(new ExpressError(404, "Page not found!"));
+});
+
+// Error handling - middleware
+app.use((err,req,res,next) => {
+    let {status = 500, message="Something went Wrong!"} = err;
+    res.status(status).send(message);
 });
 
 app.listen(port, (req,res) => {
